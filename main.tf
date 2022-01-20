a9a1e4e004b65ae4b6b9219627fb748a7e3ac9ac
armalite <ttc_rulz@hotmail.com>
2022-01-21 08:22:19 +1300

    Separate locals from main. Add second instance of module call in root module to show more examples

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "grant-me-this" {
+module "grant-for-beautiful-db" {
   source = "./sf-db-grants"
   providers = {
     snowflake.ACCOUNTADMIN  = snowflake.ACCOUNTADMIN
@@ -28,6 +28,61 @@ module "grant-me-this" {
         references = {
           privilege = "REFERENCES"
           roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"]
+        }
+      }
+      view_privileges = { # the sf view grant privileges
+        select = {
+          privilege = "SELECT"
+          roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"]
+        }
+        references = {
+          privilege = "REFERENCES"
+          roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE", "BITTER_SNOWFLAKE_ROLE"]
+        }
+      }
+    }
+  }
+}
+
+
+module "grant-for-dapper-db" {
+  source = "./sf-db-grants"
+  providers = {
+    snowflake.ACCOUNTADMIN  = snowflake.ACCOUNTADMIN
+    snowflake.SECURITYADMIN = snowflake.SECURITYADMIN
+  }
+
+  grants = {
+    DAPPER_DB = { # the database for these grants
+      name = "dapper_db"
+      db_privileges = { # the sf db grant privileges
+        usage = {
+          privilege = "USAGE"                                  # a privilege
+          roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE", "SOUR_SNOWFLAKE_ROLE"] # a list of roles the privilege will be granted to
+        }
+        create_schema= {
+          privilege = "CREATE SCHEMA"
+          roles     = ["SOME_ADMIN_ROLE"]
+        }
+      }
+      schema_privileges = { # the sf schema grant privileges
+        usage = {
+          privilege = "USAGE"
+          roles     = ["SALTY_SNOWFLAKE_ROLE"]
+        }
+      }
+      table_privileges = { # the sf table grant privileges
+        select = {
+          privilege = "SELECT"
+          roles     = ["SALTY_SNOWFLAKE_ROLE", "SOUR_SNOWFLAKE_ROLE"]
+        }
+        references = {
+          privilege = "REFERENCES"
+          roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"]
+        }
+        delete = {
+          privilege = "DELETE"
+          roles     = ["SOME_ADMIN_ROLE"]
         }
       }
       view_privileges = { # the sf view grant privileges
