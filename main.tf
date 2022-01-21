5e9330a4b4de867b9e5e4263bfed4bb610a01d57
armalite <ttc_rulz@hotmail.com>
2022-01-22 11:11:49 +1300

    Empty list showing shares declaration in root module

--- main.tf
+++ main.tf
@@ -12,32 +12,38 @@ module "grant-for-beautiful-db" {
         usage = {
           privilege = "USAGE"                                  # a privilege
           roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"] # a list of roles the privilege will be granted to
+          shares    = []
         }
       }
       schema_privileges = { # the sf schema grant privileges
         usage = {
           privilege = "USAGE"
           roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"]
+          shares    = []
         }
       }
       table_privileges = { # the sf table grant privileges
         select = {
           privilege = "SELECT"
           roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"]
+          shares    = []
         }
         references = {
           privilege = "REFERENCES"
           roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"]
+          shares    = []
         }
       }
       view_privileges = { # the sf view grant privileges
         select = {
           privilege = "SELECT"
           roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE"]
+          shares    = []
         }
         references = {
           privilege = "REFERENCES"
           roles     = ["SWEET_SNOWFLAKE_ROLE", "SALTY_SNOWFLAKE_ROLE", "BITTER_SNOWFLAKE_ROLE"]
+          shares    = []
         }
       }
     }
